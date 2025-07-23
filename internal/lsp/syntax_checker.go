4f1275c669d2eb61a9030ddfe8fa8979c790f1a6
onlyati <onlyati+server@pm.me>
2025-07-23 23:41:14 +0200

    [syntax] add feature with qsr001, qsr002, qsr003

---
+++ internal/lsp/syntax_checker.go
@@ -0,0 +1,30 @@
+package lsp
+
+import (
+	"github.com/onlyati/quadlet-lsp/internal/syntax"
+	"github.com/tliron/glsp"
+	protocol "github.com/tliron/glsp/protocol_3_16"
+)
+
+func SyntaxCheckOnSave(context *glsp.Context, params *protocol.DidSaveTextDocumentParams) error {
+	uri := string(params.TextDocument.URI)
+	text := documents.read(uri)
+
+	checker := syntax.NewSyntaxChecker(text)
+
+	// Run all syntax checker rule
+	diags := checker.RunAll()
+	if len(diags) > 0 {
+		context.Notify(protocol.ServerTextDocumentPublishDiagnostics, protocol.PublishDiagnosticsParams{
+			URI:         protocol.DocumentUri(uri),
+			Diagnostics: diags,
+		})
+	} else {
+		context.Notify(protocol.ServerTextDocumentPublishDiagnostics, protocol.PublishDiagnosticsParams{
+			URI:         protocol.DocumentUri(uri),
+			Diagnostics: []protocol.Diagnostic{},
+		})
+	}
+
+	return nil
+}
---
+++ internal/syntax/main.go
@@ -0,0 +1,57 @@
+package syntax
+
+import (
+	"sync"
+
+	protocol "github.com/tliron/glsp/protocol_3_16"
+)
+
+type SyntaxChecker struct {
+	documentText string
+	hintDiag     protocol.DiagnosticSeverity
+	infoDiag     protocol.DiagnosticSeverity
+	warnDiag     protocol.DiagnosticSeverity
+	errDiag      protocol.DiagnosticSeverity
+	checks       []func(SyntaxChecker) []protocol.Diagnostic
+}
+
+func NewSyntaxChecker(documentText string) SyntaxChecker {
+	return SyntaxChecker{
+		documentText: documentText,
+		hintDiag:     protocol.DiagnosticSeverityHint,
+		infoDiag:     protocol.DiagnosticSeverityInformation,
+		warnDiag:     protocol.DiagnosticSeverityWarning,
+		errDiag:      protocol.DiagnosticSeverityError,
+		checks: []func(SyntaxChecker) []protocol.Diagnostic{
+			qsr001,
+			qsr002,
+			qsr003,
+		},
+	}
+}
+
+func (s SyntaxChecker) RunAll() []protocol.Diagnostic {
+	var wg sync.WaitGroup
+	diagChan := make(chan []protocol.Diagnostic, len(s.checks))
+
+	for _, fn := range s.checks {
+		wg.Add(1)
+		go func(rule func(SyntaxChecker) []protocol.Diagnostic) {
+			defer wg.Done()
+			result := fn(s)
+			if result != nil {
+				diagChan <- result
+			}
+		}(fn)
+	}
+
+	wg.Wait()
+	close(diagChan)
+
+	var allDiags []protocol.Diagnostic
+	for diags := range diagChan {
+		allDiags = append(allDiags, diags...)
+	}
+
+	return allDiags
+}
