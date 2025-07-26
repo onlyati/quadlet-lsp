412b54b019e6a7613b9776c299286c6c13037157
onlyati <onlyati+server@pm.me>
2025-07-26 16:12:34 +0200

    [syntax-qsr006] implemented

--- internal/syntax/qsr006.go
+++ internal/syntax/qsr006.go
@@ -1,6 +1,15 @@
 package syntax
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"slices"
+	"strings"
+
+	"github.com/onlyati/quadlet-lsp/internal/utils"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 )
 
@@ -9,7 +18,44 @@ import (
 func qsr006(s SyntaxChecker) []protocol.Diagnostic {
 	var diags []protocol.Diagnostic
 
-	// findings := utils.FindItems(s.documentText, "Container", "Image")
+	allowedFiles := []string{"container", "volume"}
+	tmp := strings.Split(s.uri, ".")
+	ext := tmp[len(tmp)-1]
+	if !slices.Contains(allowedFiles, ext) {
+		return diags
+	}
+
+	findings := utils.FindItems(
+		s.documentText,
+		utils.FirstCharacterToUpper(ext),
+		"Image",
+	)
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Printf("failed to detect cwd: %s", err.Error())
+		return diags
+	}
+
+	for _, finding := range findings {
+		if !strings.HasSuffix(finding.Value, ".image") && !strings.HasSuffix(finding.Value, ".build") {
+			continue
+		}
+
+		filePath := path.Join(cwd, finding.Value)
+		_, err := os.Stat(filePath)
+
+		if errors.Is(err, os.ErrNotExist) {
+			diags = append(diags, protocol.Diagnostic{
+				Range: protocol.Range{
+					Start: protocol.Position{Line: finding.LineNumber, Character: 0},
+					End:   protocol.Position{Line: finding.LineNumber, Character: finding.Length},
+				},
+				Severity: &s.errDiag,
+				Source:   utils.ReturnAsStringPtr("quadlet-lsp.qsr006"),
+				Message:  fmt.Sprintf("Image file does not exists: %s", finding.Value),
+			})
+		}
+	}
 
 	return diags
 }
