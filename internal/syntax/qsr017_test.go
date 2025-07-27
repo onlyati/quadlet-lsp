96059024e0e07d8d15913672b8cc7a16dc87c348
onlyati <onlyati+server@pm.me>
2025-07-27 21:44:24 +0200

    [syntax-qsr017] implemented

---
+++ internal/syntax/qsr017_test.go
@@ -0,0 +1,61 @@
+package syntax
+
+import (
+	"os"
+	"testing"
+)
+
+func TestQSR017_Valid(t *testing.T) {
+	tmpDir := t.TempDir()
+	os.Chdir(tmpDir)
+
+	createTempFile(
+		t,
+		tmpDir,
+		"test.pod",
+		"[Pod]",
+	)
+
+	cases := []SyntaxChecker{
+		NewSyntaxChecker(
+			"[Container]\nPod=test.pod",
+			"file://"+tmpDir+"/test1.container",
+		),
+	}
+
+	for _, s := range cases {
+		diags := qsr017(s)
+
+		if len(diags) != 0 {
+			t.Fatalf("Expected 0 diagnostics, got %d at %s", len(diags), s.uri)
+		}
+	}
+}
+
+func TestQSR017_Invalid(t *testing.T) {
+	tmpDir := t.TempDir()
+	os.Chdir(tmpDir)
+
+	cases := []SyntaxChecker{
+		NewSyntaxChecker(
+			"[Container]\nPod=test.pod",
+			"file://"+tmpDir+"/test1.container",
+		),
+	}
+
+	for _, s := range cases {
+		diags := qsr017(s)
+
+		if len(diags) != 1 {
+			t.Fatalf("Expected 1 diagnostics, got %d at %s", len(diags), s.uri)
+		}
+
+		if *diags[0].Source != "quadlet-lsp.qsr017" {
+			t.Fatalf("Wrong source found: %s at %s", *diags[0].Source, s.uri)
+		}
+
+		if diags[0].Message != "Pod file does not exists: test.pod" {
+			t.Fatalf("Unexpected message: '%s' at %s", diags[0].Message, s.uri)
+		}
+	}
+}
