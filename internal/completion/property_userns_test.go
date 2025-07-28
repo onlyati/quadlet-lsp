fbcbca859de78874a3561600e05578d1b45adf03
onlyati <onlyati+server@pm.me>
2025-07-28 23:26:07 +0200

    [completion] userns implemented, list properties implemented

---
+++ internal/completion/property_userns_test.go
@@ -0,0 +1,77 @@
+package completion
+
+import (
+	"slices"
+	"testing"
+)
+
+type usernsMockCommander struct{}
+
+func (m usernsMockCommander) Run(name string, args ...string) ([]string, error) {
+	if args[2] == "scr.io/org/mock1:latest" {
+		return []string{
+			"[",
+			"	{",
+			"		 \"Config\": {",
+			"			\"User\": \"999\" ",
+			"		 }",
+			"	}",
+			"]",
+		}, nil
+	}
+
+	return []string{}, nil
+}
+
+func TestPropertyUserIDs_Valid(t *testing.T) {
+	s := NewCompletion(
+		[]string{"[Container]", "UserNS=keep-id:", "Image=scr.io/org/mock1:latest"},
+		"foo.container",
+		1,
+		0,
+	)
+	s.commander = usernsMockCommander{}
+
+	comps := propertyListUserIDs(s)
+
+	labels := []string{}
+	for _, c := range comps {
+		labels = append(labels, c.Label)
+	}
+
+	if len(labels) != 2 {
+		t.Fatalf("exptected 2 completions, but got %d", len(labels))
+	}
+
+	checkGid := slices.Contains(labels, "gid=999")
+	checkUid := slices.Contains(labels, "uid=999")
+	if !checkGid || !checkUid {
+		t.Fatalf(
+			"did not read correct values: %v %v %v",
+			labels,
+			checkGid,
+			checkUid,
+		)
+	}
+}
+
+func TestPropertyUserIDs_Invalid(t *testing.T) {
+	s := NewCompletion(
+		[]string{"[Container]", "UserNS=auto", "Image=scr.io/org/mock1:latest"},
+		"foo.container",
+		1,
+		0,
+	)
+	s.commander = usernsMockCommander{}
+
+	comps := propertyListUserIDs(s)
+
+	labels := []string{}
+	for _, c := range comps {
+		labels = append(labels, c.Label)
+	}
+
+	if len(labels) != 0 {
+		t.Fatalf("exptected 0 completions, but got %d", len(labels))
+	}
+}
