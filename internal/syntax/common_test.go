50b2f8b035193921046e9f7611eb0beebcbc360c
onlyati <onlyati+github@proton.me>
2025-09-09 21:24:49 +0200

    [feature] Add drop-in file support (#95)

--- internal/syntax/common_test.go
+++ internal/syntax/common_test.go
@@ -10,12 +10,27 @@ func TestCanFileBeApplied_Valid(t *testing.T) {
 		t.Fatalf("expected 'Container', got '%s'", tmp)
 	}
 
+	tmp = canFileBeApplied("file:///foo.container.d/10-ports.conf", list)
+	if tmp != "[Container]" {
+		t.Fatalf("expected 'Container', got '%s'", tmp)
+	}
+
 	tmp = canFileBeApplied("file:///foo.image", list)
 	if tmp != "[Image]" {
 		t.Fatalf("expected 'Image', got '%s'", tmp)
 	}
 
+	tmp = canFileBeApplied("file:///foo.image.d/auth.conf", list)
+	if tmp != "[Image]" {
+		t.Fatalf("expected 'Image', got '%s'", tmp)
+	}
+
 	tmp = canFileBeApplied("file:///foo.volume", list)
+	if tmp != "[Volume]" {
+		t.Fatalf("expected 'Volume', got '%s'", tmp)
+	}
+
+	tmp = canFileBeApplied("file:///foo.volume.d/driver.conf", list)
 	if tmp != "[Volume]" {
 		t.Fatalf("expected 'Volume', got '%s'", tmp)
 	}
@@ -25,6 +40,11 @@ func TestCanFileBeApplied_Invalid(t *testing.T) {
 	list := []string{"container", "volume", "image"}
 
 	tmp := canFileBeApplied("file:///foo.network", list)
+	if tmp != "" {
+		t.Fatalf("expected '', got '%s'", tmp)
+	}
+
+	tmp = canFileBeApplied("file:///foo.conf", list)
 	if tmp != "" {
 		t.Fatalf("expected '', got '%s'", tmp)
 	}
