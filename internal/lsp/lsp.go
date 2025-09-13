634878783f32ee1ff13674cb5b8350d477b4360f
onlyati <onlyati+github@proton.me>
2025-09-13 23:45:28 +0200

    [feature] Add release packages (#102)

--- internal/lsp/lsp.go
+++ internal/lsp/lsp.go
@@ -23,7 +23,7 @@ import (
 const lsName = "quadlet"
 
 var (
-	version   = "0.4.0"
+	version   = "0.5.0rc1"
 	handler   protocol.Handler
 	config    *utils.QuadletConfig
 	documents = utils.NewDocuments()
