9b36ebbc954ab0db8b90fc9cb4269387e537785b
onlyati <onlyati+github@proton.me>
2025-09-20 20:29:44 +0200

    [repo] fixing action

--- internal/syntax/qsr025.go
+++ internal/syntax/qsr025.go
@@ -2,6 +2,7 @@ package syntax
 
 import protocol "github.com/tliron/glsp/protocol_3_16"
 
+// Verify if image is present in container Quadlets
 func qsr025(s SyntaxChecker) []protocol.Diagnostic {
 	var diags []protocol.Diagnostic
 
