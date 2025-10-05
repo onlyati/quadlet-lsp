1ba0a70114d223d7c7d5e42ac1e3e28853f92353
onlyati <onlyati+github@proton.me>
2025-10-05 21:48:44 +0200

    [feature] First basic implementation of command

--- internal/commands/generate_doc.go
+++ internal/commands/generate_doc.go
@@ -1,6 +1,12 @@
 package commands
 
-import "github.com/onlyati/quadlet-lsp/internal/utils"
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/onlyati/quadlet-lsp/internal/utils"
+	"github.com/onlyati/quadlet-lsp/pkg/parser"
+)
 
 func generateDoc(
 	command string,
@@ -8,4 +14,29 @@ func generateDoc(
 	messenger utils.Messenger,
 	executor utils.Commander,
 ) {
+	e.mutex.Lock()
+	rootDir := e.rootDir
+	e.mutex.Unlock()
+
+	qd, err := parser.ParseQuadletDir(rootDir)
+	if err != nil {
+		messenger.SendMessage(
+			utils.MessengerError,
+			"failed to generate doc: "+err.Error(),
+		)
+	}
+
+	file, err := json.Marshal(qd)
+	if err != nil {
+		messenger.SendMessage(
+			utils.MessengerError,
+			"failed to generate doc: "+err.Error(),
+		)
+	}
+	os.WriteFile("./data.json", file, 0o644)
+
+	messenger.SendMessage(
+		utils.MessengerInfo,
+		"Document is generated",
+	)
 }
