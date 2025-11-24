0b7f20d99604c66dac6e97fc351fabe3579ef7b3
onlyati <onlyati+github@proton.me>
2025-11-24 15:47:46 +0100

    [qa] Write tests for the cases

--- internal/lsp/go_references_test.go
+++ internal/lsp/go_references_test.go
@@ -2,6 +2,7 @@ package lsp
 
 import (
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -13,6 +14,14 @@ func createTempFile(t *testing.T, dir, name, content string) string {
 	t.Helper()
 	path := filepath.Join(dir, name)
 	err := os.WriteFile(path, []byte(content), 0o644)
+	assert.NoError(t, err)
+	return path
+}
+
+func createTempDir(t *testing.T, dir, name string) string {
+	t.Helper()
+	path := filepath.Join(dir, name)
+	err := os.Mkdir(path, 0o755)
 	assert.NoError(t, err)
 	return path
 }
@@ -34,7 +43,7 @@ func TestFindReferences(t *testing.T) {
 	assert.Equal(t, uint32(1), locations[0].Range.Start.Line)
 }
 
-func TestFiindReferencesTemplate(t *testing.T) {
+func TestFindReferencesTemplate(t *testing.T) {
 	tmpDir := t.TempDir()
 	_ = os.Chdir(tmpDir)
 
@@ -54,4 +63,45 @@ func TestFiindReferencesTemplate(t *testing.T) {
 			t.Fatalf("Unexpected finding: %+v", loc)
 		}
 	}
+}
+
+func TestFindReferencesDropIns(t *testing.T) {
+	tmpDir := t.TempDir()
+	_ = os.Chdir(tmpDir)
+
+	createTempFile(t, tmpDir, "foo.container", "[Container]\nImage=foo.image\n")
+	createTempFile(t, tmpDir, "foo.pod", "[Pod]\n")
+	createTempDir(t, tmpDir, "foo.container.d")
+	createTempFile(t, path.Join(tmpDir, "foo.container.d"), "pod.conf", "[Container]\nPod=foo.pod\n")
+
+	locations, err := findReferences(
+		goReferenceProperty{
+			property: "Pod",
+			searchIn: []string{"container", "kube", "volume", "network", "image", "build"},
+		}, "foo.pod", tmpDir)
+	assert.NoError(t, err)
+	assert.Len(t, locations, 1)
+	assert.Contains(t, string(locations[0].URI), "foo.container.d/pod.conf")
+	assert.Equal(t, uint32(1), locations[0].Range.Start.Line)
+}
+
+func TestFindReferencesNested(t *testing.T) {
+	tmpDir := t.TempDir()
+	_ = os.Chdir(tmpDir)
+
+	createTempDir(t, tmpDir, "foo")
+	createTempFile(t, path.Join(tmpDir, "foo"), "foo.container", "[Container]\nImage=foo.image\n")
+	createTempFile(t, path.Join(tmpDir, "foo"), "foo.pod", "[Pod]\n")
+	createTempDir(t, path.Join(tmpDir, "foo"), "foo.container.d")
+	createTempFile(t, path.Join(tmpDir, "foo", "foo.container.d"), "pod.conf", "[Container]\nPod=foo.pod\n")
+
+	locations, err := findReferences(
+		goReferenceProperty{
+			property: "Pod",
+			searchIn: []string{"container", "kube", "volume", "network", "image", "build"},
+		}, "foo.pod", tmpDir)
+	assert.NoError(t, err)
+	assert.Len(t, locations, 1)
+	assert.Contains(t, string(locations[0].URI), "foo/foo.container.d/pod.conf")
+	assert.Equal(t, uint32(1), locations[0].Range.Start.Line)
 }
