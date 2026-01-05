64f87ef82ef3f22f35363c3c25a738c4b7836a27
onlyati <onlyati+github@proton.me>
2026-01-05 22:38:15 +0100

    add walkdir function with level parameter

--- internal/utils/general.go
+++ internal/utils/general.go
@@ -5,6 +5,7 @@ package utils
 
 import (
 	"io/fs"
+	"os"
 	"path/filepath"
 	"strings"
 	"unicode"
@@ -17,15 +18,42 @@ func ReturnAsStringPtr(s string) *string {
 	return &s
 }
 
-// Return pointer of any
+// ReturnAsPtr return with pointer of any type
 func ReturnAsPtr[T any](s T) *T {
 	return &s
 }
 
+// FirstCharacterToUpper makes the first character upper case
 func FirstCharacterToUpper(s string) string {
 	runes := []rune(s)
 	runes[0] = unicode.ToUpper(runes[0])
 	return string(runes)
+}
+
+func QuadletWalkDir(root string, level int, fn fs.WalkDirFunc) error {
+	return filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		tmp := strings.TrimPrefix(path, root)
+		tmp = strings.TrimPrefix(tmp, string(os.PathSeparator))
+
+		// Ignore hidden directories
+		if d.IsDir() && strings.HasPrefix(tmp, ".") {
+			return filepath.SkipDir
+		}
+
+		// Check how deep we are
+		numberOfSlices := strings.Count(tmp, string(os.PathSeparator))
+		if numberOfSlices > level {
+			if d.IsDir() {
+				return filepath.SkipDir
+			} else {
+				return nil
+			}
+		}
+
+		// Call the original function
+		fn(path, d, err)
+		return nil
+	})
 }
 
 // ListQuadletFiles List quadlet files from the current work directory based on extenstion
