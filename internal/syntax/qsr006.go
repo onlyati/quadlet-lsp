bfec685872b40c8be32fd445cf09866fe4327a14
onlyati <onlyati+github@proton.me>
2026-02-08 17:36:25 +0100

    [fix] QSR006 looking for in nested directories

--- internal/syntax/qsr006.go
+++ internal/syntax/qsr006.go
@@ -1,12 +1,10 @@
 package syntax
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"os"
-	"path"
 	"regexp"
+	"strings"
 
 	"github.com/onlyati/quadlet-lsp/internal/utils"
 	protocol "github.com/tliron/glsp/protocol_3_16"
@@ -14,12 +12,23 @@ import (
 
 var qsr006AnotherQuadlet = regexp.MustCompile(`^[a-zA-Z0-9][a-zA-Z0-9_.-]*\.(image|build)$`)
 
+type qsr006ActionParms struct {
+	rootDir  string
+	dirLevel int
+}
+
 // Verify if the specified `.image` or `.build` file exists
 // in the current working directory
 func qsr006(s SyntaxChecker) []protocol.Diagnostic {
 	var diags []protocol.Diagnostic
 
 	allowedFiles := []string{"container", "volume"}
+	s.config.Mu.RLock()
+	parm := qsr017ActionParms{
+		rootDir:  s.config.WorkspaceRoot,
+		dirLevel: *s.config.Project.DirLevel,
+	}
+	s.config.Mu.RUnlock()
 
 	if c := canFileBeApplied(s.uri, allowedFiles); c != "" {
 		diags = utils.ScanQadlet(
@@ -29,39 +38,54 @@ func qsr006(s SyntaxChecker) []protocol.Diagnostic {
 				{Section: c, Property: "Image"}: {},
 			},
 			qsr006Action,
-			nil,
+			parm,
 		)
 	}
 
 	return diags
 }
 
-func qsr006Action(q utils.QuadletLine, _ utils.PodmanVersion, _ any) []protocol.Diagnostic {
-	if !qsr006AnotherQuadlet.MatchString(q.Value) {
+func qsr006Action(q utils.QuadletLine, _ utils.PodmanVersion, extraInfo any) []protocol.Diagnostic {
+	actionParm := qsr017ActionParms{}
+	switch v := extraInfo.(type) {
+	case qsr017ActionParms:
+		actionParm = v
+	default:
 		return nil
 	}
 
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Printf("failed to detect cwd: %s", err.Error())
-		return nil
-	}
-	filePath := path.Join(cwd, q.Value)
-	_, err = os.Stat(filePath)
+	imageName := q.Value
+	ext := q.Value[strings.LastIndexAny(q.Value, ".")+1:]
+	if qsr006AnotherQuadlet.MatchString(q.Value) {
+		quadlets, err := utils.ListQuadletFiles(ext, actionParm.rootDir, actionParm.dirLevel)
+		exists := false
 
-	if !errors.Is(err, os.ErrNotExist) {
-		return nil
+		for _, q := range quadlets {
+			if imageName == q.Label {
+				exists = true
+				break
+			}
+		}
+
+		if !exists {
+			return []protocol.Diagnostic{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: q.LineNumber, Character: uint32(len(q.Property) + 1)},
+						End:   protocol.Position{Line: q.LineNumber, Character: uint32(len(q.Property) + 1 + len(imageName))},
+					},
+					Severity: &errDiag,
+					Source:   utils.ReturnAsStringPtr("quadlet-lsp.qsr006"),
+					Message:  fmt.Sprintf("Image file does not exists: %s", imageName),
+				},
+			}
+		}
+
+		if err != nil {
+			log.Printf("failed to stat file: %s", err.Error())
+			return nil
+		}
 	}
 
-	return []protocol.Diagnostic{
-		{
-			Range: protocol.Range{
-				Start: protocol.Position{Line: q.LineNumber, Character: 0},
-				End:   protocol.Position{Line: q.LineNumber, Character: q.Length},
-			},
-			Severity: &errDiag,
-			Source:   utils.ReturnAsStringPtr("quadlet-lsp.qsr006"),
-			Message:  fmt.Sprintf("Image file does not exists: %s", q.Value),
-		},
-	}
+	return nil
 }
--- internal/syntax/qsr006_test.go
+++ internal/syntax/qsr006_test.go
@@ -1,61 +1,90 @@
 package syntax
 
 import (
-	"os"
-	"path/filepath"
+	"path"
 	"testing"
 
 	"github.com/onlyati/quadlet-lsp/internal/testutils"
+	"github.com/onlyati/quadlet-lsp/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func createTestFile(t *testing.T, dir, name, content string) string {
-	t.Helper()
-	path := filepath.Join(dir, name)
-	err := os.WriteFile(path, []byte(content), 0o644)
-	assert.NoError(t, err)
-	return path
-}
-
 func TestQSR006_Valid(t *testing.T) {
 	tmpDir := t.TempDir()
-	_ = os.Chdir(tmpDir)
 
 	testutils.CreateTempFile(t, tmpDir, "foo.image", "[Image]\nImage=docker.io/library/debian")
 
 	s := NewSyntaxChecker("[Container]\nImage=foo.image", "foo.container")
+	s.config = &utils.QuadletConfig{
+		WorkspaceRoot: tmpDir,
+		Project: utils.ProjectProperty{
+			DirLevel: utils.ReturnAsPtr(2),
+		},
+	}
 	diags := qsr006(s)
 	require.Len(t, diags, 0)
 }
 
 func TestQSR006_ValidVolume(t *testing.T) {
 	tmpDir := t.TempDir()
-	_ = os.Chdir(tmpDir)
+
+	testutils.CreateTempDir(t, tmpDir, "foo")
+	testutils.CreateTempFile(t, path.Join(tmpDir, "foo"), "foo.image", "[Image]\nImage=docker.io/library/debian")
+
+	s := NewSyntaxChecker("[Volume]\nImage=foo.image", "foo.volume")
+	s.config = &utils.QuadletConfig{
+		WorkspaceRoot: tmpDir,
+		Project: utils.ProjectProperty{
+			DirLevel: utils.ReturnAsPtr(2),
+		},
+	}
+	diags := qsr006(s)
+	require.Len(t, diags, 0)
+}
+
+func TestQSR006_ValidVolumeNested(t *testing.T) {
+	tmpDir := t.TempDir()
 
 	testutils.CreateTempFile(t, tmpDir, "foo.image", "[Image]\nImage=docker.io/library/debian")
 
 	s := NewSyntaxChecker("[Volume]\nImage=foo.image", "foo.volume")
+	s.config = &utils.QuadletConfig{
+		WorkspaceRoot: tmpDir,
+		Project: utils.ProjectProperty{
+			DirLevel: utils.ReturnAsPtr(2),
+		},
+	}
 	diags := qsr006(s)
 	require.Len(t, diags, 0)
 }
 
 func TestQSR006_Skipped(t *testing.T) {
 	tmpDir := t.TempDir()
-	_ = os.Chdir(tmpDir)
 
 	s := NewSyntaxChecker("[Container]\nImage=library/debian", "foo.container")
+	s.config = &utils.QuadletConfig{
+		WorkspaceRoot: tmpDir,
+		Project: utils.ProjectProperty{
+			DirLevel: utils.ReturnAsPtr(2),
+		},
+	}
 	diags := qsr006(s)
 	require.Len(t, diags, 0)
 }
 
 func TestQSR006_Invalid(t *testing.T) {
 	tmpDir := t.TempDir()
-	_ = os.Chdir(tmpDir)
 
-	createTestFile(t, tmpDir, "foo.image", "[Image]\nImage=docker.io/library/debian")
+	testutils.CreateTempFile(t, tmpDir, "foo.image", "[Image]\nImage=docker.io/library/debian")
 
 	s := NewSyntaxChecker("[Container]\nImage=bar.image", "foo.container")
+	s.config = &utils.QuadletConfig{
+		WorkspaceRoot: tmpDir,
+		Project: utils.ProjectProperty{
+			DirLevel: utils.ReturnAsPtr(2),
+		},
+	}
 	diags := qsr006(s)
 	require.Len(t, diags, 1)
 	require.NotNil(t, diags[0].Source)
