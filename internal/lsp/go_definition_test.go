48bcb4557106a7e1a9588eaa611d31c7fd08e9db
onlyati <onlyati+github@proton.me>
2026-02-08 12:38:39 +0100

    reviewed findQuadlet tests

--- internal/lsp/go_definition_test.go
+++ internal/lsp/go_definition_test.go
@@ -1,26 +1,16 @@
 package lsp
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
+	"github.com/onlyati/quadlet-lsp/internal/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
-func createTestFile(t *testing.T, dir, name, content string) string {
-	t.Helper()
-	path := filepath.Join(dir, name)
-	err := os.WriteFile(path, []byte(content), 0o644)
-	assert.NoError(t, err)
-	return path
-}
-
 func TestFindQuadlets_MatchingFile(t *testing.T) {
 	tmpDir := t.TempDir()
-	_ = os.Chdir(tmpDir)
 
-	createTestFile(t, tmpDir, "mypod.pod", "dummy content")
+	testutils.CreateTempFile(t, tmpDir, "mypod.pod", "dummy content")
 
 	loc, err := findQuadlets("pod", "mypod.pod", tmpDir, 2)
 	assert.NoError(t, err)
@@ -29,9 +19,8 @@ func TestFindQuadlets_MatchingFile(t *testing.T) {
 
 func TestFindQuadlets_VolumeColonSuffix(t *testing.T) {
 	tmpDir := t.TempDir()
-	_ = os.Chdir(tmpDir)
 
-	createTestFile(t, tmpDir, "myvol.volume", "dummy content")
+	testutils.CreateTempFile(t, tmpDir, "myvol.volume", "dummy content")
 
 	loc, err := findQuadlets("volume", "myvol.volume:ro", tmpDir, 2)
 	assert.NoError(t, err)
@@ -40,9 +29,8 @@ func TestFindQuadlets_VolumeColonSuffix(t *testing.T) {
 
 func TestFindQuadlets_NoMatch(t *testing.T) {
 	tmpDir := t.TempDir()
-	_ = os.Chdir(tmpDir)
 
-	createTestFile(t, tmpDir, "somethingelse.network", "dummy content")
+	testutils.CreateTempFile(t, tmpDir, "somethingelse.network", "dummy content")
 
 	loc, err := findQuadlets("network", "notfound.network", tmpDir, 2)
 	assert.NoError(t, err)
